8fcdbe3dd762132fa636d98f1cf2fc1722034138
joshxb2 <algadipej962@gmail.com>
2025-02-05 17:55:12 +0800

    add sitemap.xml and robots.txt (seo)

--- flask/app.py
+++ flask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template # type: ignore
+from flask import Flask, render_template, send_from_directory # type: ignore
 from config import (
     ERROR_404_PAGE,
     LANGUAGES,
@@ -11,13 +11,16 @@ from config import (
 from flask_babel import Babel  # type: ignore
 from pages import pages_bp 
 from datetime import datetime
+from sitemap import sitemap_bp  # Import the sitemap blueprint
+import os
 
 app = Flask(__name__)
 
 app.config["LANGUAGES"] = LANGUAGES
 app.config["BABEL_DEFAULT_LOCALE"] = BABEL_DEFAULT_LOCALE
 app.register_blueprint(pages_bp)
-
+# Register the sitemap blueprint
+app.register_blueprint(sitemap_bp)
 
 babel = Babel(app)
 babel.init_app(app, locale_selector=get_locale)
@@ -51,6 +54,10 @@ def inject_now():
 @app.errorhandler(404)
 def not_found(error):
     return render_template(ERROR_404_PAGE, title="Not Found", error=error), 404
+
+@app.route('/robots.txt')
+def robots():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'robots.txt')
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
---
+++ flask/sitemap.py
@@ -0,0 +1,42 @@
+from flask import Blueprint, Response, request
+from datetime import datetime
+
+sitemap_bp = Blueprint("sitemap", __name__)
+
+def generate_sitemap():
+    lastmod = datetime.today().strftime('%Y-%m-%d')
+    base_url = request.host_url.rstrip('/')  # Get the current domain dynamically
+
+    sitemap_xml = f"""<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+        <url>
+            <loc>{base_url}/</loc>
+            <lastmod>{lastmod}</lastmod>
+        </url>
+        <url>
+            <loc>{base_url}/profile</loc>
+            <lastmod>{lastmod}</lastmod>
+        </url>
+        <url>
+            <loc>{base_url}/gallery</loc>
+            <lastmod>{lastmod}</lastmod>
+        </url>
+        <url>
+            <loc>{base_url}/vlog</loc>
+            <lastmod>{lastmod}</lastmod>
+        </url>
+        <url>
+            <loc>{base_url}/concern</loc>
+            <lastmod>{lastmod}</lastmod>
+        </url>
+        <url>
+            <loc>{base_url}/donation</loc>
+            <lastmod>{lastmod}</lastmod>
+        </url>
+    </urlset>"""
+    
+    return Response(sitemap_xml, mimetype="application/xml")
+
+@sitemap_bp.route("/sitemap.xml")
+def sitemap():
+    return generate_sitemap()
