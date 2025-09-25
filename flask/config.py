75d6050168e6f4ab0036621eb38c552d02936f60
joshxb2 <102468882+JPA-Elite@users.noreply.github.com>
2025-09-25 11:04:46 +0000

    added adminLTE dist

--- flask/config.py
+++ flask/config.py
@@ -27,7 +27,7 @@ DEVICE_INFO_VISIT_PAGE = "pages/visitors.html"
 ERROR_404_PAGE = "error/404.html"
 
 # admin pages
-ADMIN_DASHBOARD_PAGE = "AdminLTE/dist/index.html"
+ADMIN_DASHBOARD_PAGE = "admin/dist/index.html"
 
 LANGUAGES = {
     "en": "English",
